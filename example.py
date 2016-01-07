5411224e9683c9ee6a8b06ff9b666a93948e6a69
garygitt <m@planet.bot.nu>
2016-01-07 12:12:40 +0200

    Create example.py

---
+++ example.py
@@ -0,0 +1,19 @@
+#TABLE LOAD
+
+self.table_data = QtGui.QTableView()
+cols=['rowid','data']
+data = [(1,'data1'),(2,'data2'),]
+table.load(self.table_data,data,cols,order=0,col=0)
+
+#TABLE SORT
+
+def context(self,pos):
+		mainmenu = QtGui.QMenu("Menu", self)
+		mainmenu.addAction("Sort")
+		
+		C =  self.mapFromGlobal(QCursor.pos())
+		pos.setY(C.y()); pos.setX(C.x())
+		action  = mainmenu.exec_(self.mapToGlobal(pos))
+		
+		if action.text() == 'Sort':
+			table.sort(self.sender())
